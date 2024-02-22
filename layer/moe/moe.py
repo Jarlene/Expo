3ce3a4c3e5c377489bdd43836f8fd11301581919
Jarlene <jarlene@xxxx.com>
2024-02-22 13:45:54 +0800

    add DroplessMoE

--- layer/moe/moe.py
+++ layer/moe/moe.py
@@ -5,18 +5,19 @@ from typing import Any
 import torch
 from ..layers import RMSNorm
 
+
 class SoftMoE(nn.Module):
     def __init__(self, dim,
                  expers_num,
                  slots_num,
-                 experts: nn.Module,
+                 expert: nn.Module,
                  add_noise=True,
                  noise_scala=1.0) -> Any:
         super().__init__()
         self.norm = RMSNorm(dim)
         self.slot_embeds = nn.Parameter(
             torch.rand((expers_num, slots_num, dim)))
-        self.experts = nn.ModuleList([copy.deepcopy(experts)
+        self.experts = nn.ModuleList([copy.deepcopy(expert)
                                      for _ in range(expers_num)])
         self.add_noise = add_noise
         self.noise_scala = noise_scala
@@ -48,3 +49,64 @@ class SoftMoE(nn.Module):
         outs = torch.einsum('besd, bnes->bnd', outs, combine_weights)
         return outs
 
+
+class DroplessMoE(nn.Module):
+    def __init__(
+            self,
+            hidden_size: int,
+            num_experts: int,
+            moe_num_experts_per_token: int,
+            expert: nn.Module,):
+        super().__init__()
+        num_experts = num_experts
+        self.experts = nn.ModuleList(
+            [copy.deepcopy(expert) for i in range(num_experts)])
+        self.gate = nn.Linear(hidden_size, num_experts, bias=False)
+        self.num_experts_per_token = moe_num_experts_per_token
+
+    def load_balancing_loss(self, router_probs: torch.Tensor, expert_mask: torch.Tensor = None):
+        num_experts = router_probs.shape[-1]
+        router_prob_per_expert = torch.mean(
+            router_probs, dtype=torch.float32, dim=-2)
+
+        if expert_mask is not None:
+            expert_mask = torch.nn.functional.one_hot(
+                expert_mask, num_experts).to(torch.int32)
+            expert_mask, expert_index = torch.max(expert_mask, dim=-2)
+            tokens_per_expert = torch.mean(
+                expert_mask, dim=-2, dtype=torch.float32)
+            return torch.mean(
+                tokens_per_expert * router_prob_per_expert,
+                dtype=torch.float32) * num_experts**2
+        else:
+            return torch.mean(
+                router_prob_per_expert,
+                dtype=torch.float32) * num_experts**2
+
+    def router_z_loss(self, router_logits: torch.Tensor):
+        token_num, _ = router_logits.shape
+        log_z = torch.logsumexp(router_logits, dim=-1)
+        z_loss = log_z**2
+        return torch.sum(z_loss, dtype=torch.float32) / token_num
+
+    def computer_loss(self, router_logits: torch.Tensor, router_probs: torch.Tensor, expert_mask: torch.Tensor = None):
+        self.z_loss = self.router_z_loss(router_logits)
+        self.auxiliary_loss = self.load_balancing_loss(
+            router_probs, expert_mask)
+
+    def forward(self, x: torch.Tensor):
+        orig_shape = x.shape
+        x = x.view(-1, x.shape[-1])
+        router_logits = self.gate(x)
+        routing_weights = torch.nn.functional.softmax(router_logits, dim=-1)
+        expert_weights, expert_indices = torch.topk(
+            routing_weights, self.num_experts_per_token, dim=-1)
+        self.computer_loss(router_logits, expert_weights, expert_indices)
+        flat_expert_indices = expert_indices.view(-1)
+        x = x.repeat_interleave(self.num_experts_per_token, dim=0)
+        y = torch.zeros_like(x)
+        for i, expert in enumerate(self.experts):
+            y[flat_expert_indices == i] += expert(x[flat_expert_indices == i])
+        y = (y.view(*expert_weights.shape, -1) *
+             expert_weights.unsqueeze(-1)).sum(dim=1)
+        return y.view(*orig_shape)
