c65363da7e78c32230d065e308e6914b5b572306
Jarlene <jarlene@xiaomi.com>
2024-02-27 12:18:38 +0800

    fix moe bug

--- layer/moe/moe.py
+++ layer/moe/moe.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from typing import Any
 import torch
+import torch.nn.functional as F
 from ..layers import RMSNorm
 
 
@@ -113,32 +114,48 @@ class DroplessMoE(nn.Module):
         self.auxiliary_loss = self.load_balancing_loss(
             router_probs, expert_mask)
 
-    def forward(self, x: torch.Tensor):
-        bs, seq, hidden_dim = x.shape
-        device = x.device
-        y = x.view(-1, hidden_dim)
-        router_states = self.add_noise(y, self.training)
-        router_logits = self.gate(router_states)
-        routing_weights = torch.nn.functional.softmax(router_logits, dim=-1)
-        expert_weights, expert_indices = torch.topk(
-            routing_weights, self.num_experts_per_token, dim=-1)
+    def forward(self, hidden_states: torch.Tensor):
+        batch_size, sequence_length, hidden_dim = hidden_states.shape
+        hidden_states = hidden_states.view(-1, hidden_dim)
+        hidden_states = self.add_noise(hidden_states, self.training)
+        # router_logits: (batch * sequence_length, n_experts)
+        router_logits = self.gate(hidden_states)
 
-        self.computer_loss(router_logits, expert_weights, expert_indices)
+        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
+        routing_weights, selected_experts = torch.topk(
+            routing_weights, self.top_k, dim=-1)
+        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+        # we cast back to the input dtype
+        routing_weights = routing_weights.to(hidden_states.dtype)
+        self.computer_loss(router_logits, routing_weights, selected_experts)
 
-        top_k_routing_weights_sum = expert_weights.sum(
-            dim=-1, keepdim=True)
-        routing_weights = expert_weights / top_k_routing_weights_sum
-
+        final_hidden_states = torch.zeros(
+            (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device)
         expert_mask = torch.nn.functional.one_hot(
-            expert_indices, num_classes=self.num_experts).permute(2, 1, 0)
-        final_hidden_states = torch.zeros_like(y).view(-1, y.shape[-1])
-        for i, expert in enumerate(self.experts):
-            expert = expert.to(device)
-            idx, top_x = torch.where(expert_mask[i])
+            selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+        for expert_idx in range(self.num_experts):
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx])
+
+            if top_x.shape[0] == 0:
+                continue
+
+            # in torch it is faster to index using lists than torch tensors
             top_x_list = top_x.tolist()
             idx_list = idx.tolist()
-            z = y[None, top_x_list].reshape(-1, hidden_dim)
-            z = expert(z)
-            z = z * routing_weights[top_x_list, idx_list, None]
-            final_hidden_states.index_add_(0, top_x, z)
-        return final_hidden_states.view(bs, seq, hidden_dim), self.z_loss, self.auxiliary_loss
+
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+            current_state = hidden_states[None,
+                                          top_x_list].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(
+                current_state, routing_weights[top_x_list, idx_list, None])
+
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(
+                0, top_x, current_hidden_states.to(hidden_states.dtype))
+        final_hidden_states = final_hidden_states.reshape(
+            batch_size, sequence_length, hidden_dim)
+        return final_hidden_states, self.auxiliary_loss, self.z_loss
